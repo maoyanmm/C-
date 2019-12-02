e77b8d608b27af27a9b8477840b7cab6b3093bc9
石帅 <804826526@qq.com>
2019-12-02 23:30:44 +0800

    又练习了：求最长公共子串

--- test_2019_12_2/test_2019_12_2/test.cpp
+++ test_2019_12_2/test_2019_12_2/test.cpp
@@ -36,3 +36,44 @@ public:
 		return result;
 	}
 };
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main()
+{
+	string str1, str2;
+	while (cin >> str1 >> str2)
+	{
+		if (str1.size() > str2.size())
+		{
+			swap(str1, str2);
+		}
+		int i = 0;
+		int j = 0;
+		int start = 0;
+		int maxlen = 0;
+		int len1 = str1.size();
+		int len2 = str2.size();
+		vector<vector<int>> vv(len1 + 1, vector<int>(len2 + 1, 0));
+		for (i = 1; i <= len1; ++i)
+		{
+			for (j = 1; j <= len2; ++j)
+			{
+				if (str1[i - 1] == str2[j - 1])
+				{
+					vv[i][j] = vv[i - 1][j - 1] + 1;
+				}
+				if (maxlen < vv[i][j])
+				{
+					maxlen = vv[i][j];
+					start = i - maxlen;
+				}
+			}
+		}
+		cout << str1.substr(start, maxlen) << endl;
+	}
+	return 0;
+}
